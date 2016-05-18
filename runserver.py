4a213f42bec61a76291b8396deef4afc58e74989
Гойда Игорь Викторович <igoyda@office.finam.ru>
2016-05-18 13:34:25 +0300

    Runnable site

--- runserver.py
+++ runserver.py
@@ -7,8 +7,11 @@ from frontui import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
+    MODE = environ.get('APP_MODE', 'DEBUG')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
+    if MODE == 'DEBUG':
+        app.debug = True
     app.run(HOST, PORT)
