ca1e18af84e50f6fcee19638de32f49bf34446bd
nixxa <nixxa@mail.ru>
2016-05-28 23:33:58 +0300

    Fix #9

--- frontui/__init__.py
+++ frontui/__init__.py
@@ -5,7 +5,7 @@ import logging
 import json
 from flask import Flask
 from frontui.data import DataProvider
-#from frontui.models import ObjectInfo, ChecklistInfo
+from frontui.models import ObjectInfo, ChecklistInfo
 # create an application
 app = Flask(__name__)
 # set prod logging
@@ -14,9 +14,9 @@ app.logger.setLevel(logging.DEBUG)
 # create data provider
 DATA = DataProvider()
 # fill data from files
-#with open('./app_data/objects.json', 'r') as objects_file:
-#    for item in json.load(objects_file):
-#        DATA.add_object(ObjectInfo(item))
+with open('./app_data/objects.json', 'r') as objects_file:
+    for item in json.load(objects_file):
+        DATA.add_object(ObjectInfo(item))
 #with open('./app_data/checklist.json', 'r') as checklist_file:
 #    DATA.checklist = ChecklistInfo.from_json(json.load(checklist_file))
 
