0f21451b67acb8e7a067c7bf7439a72d1d9056ad
nixxa <nixxa@mail.ru>
2016-05-28 23:33:03 +0300

    Fix #8

--- frontui/__init__.py
+++ frontui/__init__.py
@@ -1,22 +1,16 @@
 """
 The flask application package.
 """
-#import logging
-#import json
-#import os
+import logging
+import json
 from flask import Flask
 from frontui.data import DataProvider
 #from frontui.models import ObjectInfo, ChecklistInfo
 # create an application
 app = Flask(__name__)
 # set prod logging
-#MODE = os.environ.get('APP_MODE', 'DEBUG')
-#if MODE == 'PROD':
-#    logging.basicConfig(filename='app.log', format='%(asctime)s %(message)s', level=logging.DEBUG)
-#    logging.debug('Starting app in %s mode' % MODE)
-#else:
-#    app.debug_log_format = '%(asctime)s %(levelname)s %(message)s'
-#    app.logger.setLevel(logging.DEBUG)
+app.debug_log_format = '%(asctime)s %(levelname)s %(message)s'
+app.logger.setLevel(logging.DEBUG)
 # create data provider
 DATA = DataProvider()
 # fill data from files
