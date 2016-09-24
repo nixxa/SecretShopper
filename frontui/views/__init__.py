d4392420952eede4f2448df8c9093ff064f343a2
nixxa <nixxa@mail.ru>
2016-09-24 23:11:08 +0300

    PDF added to allowed files

--- frontui/views/__init__.py
+++ frontui/views/__init__.py
@@ -3,4 +3,4 @@
 IMAGE_EXTENSIONS = set(['.png', '.jpg', '.jpeg', '.gif'])
 AUDIO_EXTENSIONS = set(['.m4a', '.wav', '.mp3', '.ogg', '.3gpp', '.mp4'])
 EXCEL_EXTENSIONS = set(['.xls', '.xlsx'])
-ALLOWED_EXTENSIONS = IMAGE_EXTENSIONS | AUDIO_EXTENSIONS | EXCEL_EXTENSIONS
+ALLOWED_EXTENSIONS = IMAGE_EXTENSIONS | AUDIO_EXTENSIONS | EXCEL_EXTENSIONS | set(['.pdf'])
